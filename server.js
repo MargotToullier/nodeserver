1755b40fb1d5913e0eb28cb79747d500c300060d
margotToullier <margottoullier@gmail.com>
2021-04-12 21:19:34 +0200

    correction

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const http = require('http');
 const url = require('url');
-const port = 8000;
+const port = 8000; 
 
+const requestHandler = (request, response) => {
 const name = 'Margot';
 const city = 'Nantes';
 const url1 = '/?name=Margot&city=Nantes';
 
 const requestHandler = (request, response) => {
-  console.log(url1);
-  response.end(`Hello ${name} from ${city}`);
+  const parsedUrl = url.parse(request.url, true);
+  console.log(parsedUrl.query);
 };
+    response.end('Hello Node.js Server!');
 const server = http.createServer(requestHandler);
 
 server.listen(port, (err) => {
