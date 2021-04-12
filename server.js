8a6c92a0e85930bb1f2cf9df4cbcd1945dd9b279
margotToullier <margottoullier@gmail.com>
2021-04-12 21:56:24 +0200

    correction2

--- server.js
+++ server.js
@@ -1,23 +1,24 @@
 const http = require('http');
 const url = require('url');
-const port = 8000; 
+
+const port = 8000;
 
 const requestHandler = (request, response) => {
-const name = 'Margot';
-const city = 'Nantes';
-const url1 = '/?name=Margot&city=Nantes';
-
-const requestHandler = (request, response) => {
-  const parsedUrl = url.parse(request.url, true);
-  console.log(parsedUrl.query);
+  console.log(request.url);
+  const nameUrl = '/?name=Ringo&city=Liverpool';
+  const parsedUrl = url.parse(nameUrl, true);
+  const data = parsedUrl.query;
+  if (data.name && data.city) {
+    response.end(`hello, ${data.name} from ${data.city}`);
+  }
 };
-    response.end('Hello Node.js Server!');
+
 const server = http.createServer(requestHandler);
 
 server.listen(port, (err) => {
   if (err) {
     console.error('Something bad happened');
   } else {
-    console.log(`Server is listening on ${port}`);
+    console.log(`server is listening on ${port}`);
   }
 });
